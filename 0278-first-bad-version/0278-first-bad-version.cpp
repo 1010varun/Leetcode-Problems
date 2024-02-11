1dc212e97a41473897fede5074c6b83e6ac3fce6
Varun Agarwal <104075461+1010varun@users.noreply.github.com>
2024-02-12 02:32:03 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (6.36%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -6,19 +6,11 @@ public:
     int firstBadVersion(int n) {
         int s = 1;
         int e = n;
-        
         while(s < e) {
             int mid = s + (e-s)/2;
-                
-            if(isBadVersion(mid) == false) {
-                s = mid + 1;
-            }
-            
-            else {
-                e = mid;
-            }
+            if(isBadVersion(mid) == false) s = mid + 1;
+            else e = mid;
         }
-        
         return s;
     }
 };
